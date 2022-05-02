cecaddc51024d75fba4a8308d3a2c747ca718b63
federica <fedhere@gmail.com>
2022-05-02 13:22:11 -0400

    Update pywordle.py

--- exercises/pywordle.py
+++ exercises/pywordle.py
@@ -8,11 +8,23 @@ simple string manipulation exercise
 
 Written by FBB April 2022 for PyBOOT
 """
-import wordle
-word = wordle.word
 
-def comparestrings(guess, word=word):
+
+def comparestrings(guess, word=None):
+  """ compares a given 5-character string with a chosen target letter by letter
+  Input:
+    guess: 5-character string - the users guess
+    word (optional): 5-character string - the target - if a string is not passed the code looks for a "wordle.py" module with the definition of word
+  Output:
+    returns a string with each correct character in the right place visible, each correct chaeracter in the wrong place marked by a "*"
+  """
   result = ['_', '_', '_', '_', '_']
+  
+  if word is None:
+    import wordle
+    word = wordle.word
+
+
   if not len(guess) == len(word):
     print("word must be 5 characters")
     return
